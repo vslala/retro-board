ed9ebb7c6a484d9c3495e1edd1a28bd5db82016e
Varun Shrivastava <varunshrivastava007@gmail.com>
2020-05-29 14:59:39 +0530

    bugfix - env-config

--- src/env-config.ts
+++ src/env-config.ts
@@ -1,21 +1,19 @@
-const API_HOST_LOCAL = 'http://localhost:8082';
-const API_HOST_TEST = 'http://139.59.25.205:8082';
-const API_HOST_PROD = 'http://139.59.25.205:8082';
+const API_HOST_LOCAL = 'localhost:8082';
+const API_HOST_TEST = '139.59.25.205:8082';
+const API_HOST_PROD = '139.59.25.205:8082';
 
 const UI_HOST_LOCAL = 'localhost:3000';
 const UI_HOST_TEST = 'localhost:3001';
 const UI_HOST_PROD = 'retro.bemyaficionado.com';
 
-const HOST_NAMES = {
-    [UI_HOST_LOCAL]: API_HOST_LOCAL,
-    [UI_HOST_TEST]: API_HOST_TEST,
-    [UI_HOST_PROD]: API_HOST_PROD,
-};
+const PROTOCOL = "http://";
+const HOST_NAMES:Map<string,string> = new Map<string,string>();
+HOST_NAMES.set(UI_HOST_LOCAL, API_HOST_LOCAL);
+HOST_NAMES.set(UI_HOST_TEST, API_HOST_TEST);
+HOST_NAMES.set(UI_HOST_PROD, API_HOST_PROD);
 
 let host = window.location.host;
-let port = window.location.port;
-let suffix = ":";
-if (port)
-    suffix = ":" + window.location.port
 // @ts-ignore
-export const SERVICE_URL = HOST_NAMES[host + suffix]; // hostname:port
+export const SERVICE_URL = PROTOCOL + HOST_NAMES.get(host); // hostname:port
+
+console.log("SERVICE_URL: ", SERVICE_URL);
