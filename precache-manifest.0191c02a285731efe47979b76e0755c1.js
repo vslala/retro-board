59d275b8f964e2a1d6809618bb7e582ef7cb384b
Varun Shrivastava <varunshrivastava007@gmail.com>
2020-09-19 15:45:58 +0530

    Updates

---
+++ precache-manifest.0191c02a285731efe47979b76e0755c1.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1f49e2354d21b8a4b033443d00475ba",
+    "url": "/retro-board/index.html"
+  },
+  {
+    "revision": "b1ccac0c6bc5ae69ce35",
+    "url": "/retro-board/static/css/2.3679ec75.chunk.css"
+  },
+  {
+    "revision": "26c0ea3e608a250a3046",
+    "url": "/retro-board/static/css/main.487eb362.chunk.css"
+  },
+  {
+    "revision": "b1ccac0c6bc5ae69ce35",
+    "url": "/retro-board/static/js/2.9b9fe6cd.chunk.js"
+  },
+  {
+    "revision": "ad41616a38793e733e3f9ed45201ee12",
+    "url": "/retro-board/static/js/2.9b9fe6cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26c0ea3e608a250a3046",
+    "url": "/retro-board/static/js/main.bd589c5e.chunk.js"
+  },
+  {
+    "revision": "2733266c25ff9c60d3bc",
+    "url": "/retro-board/static/js/runtime-main.262c44d2.js"
+  },
+  {
+    "revision": "2f75564afece714e52ab4c737c56a0c5",
+    "url": "/retro-board/static/media/bootstrap-social.2f75564a.png"
+  },
+  {
+    "revision": "1dc35d25e61d819a9c357074014867ab",
+    "url": "/retro-board/static/media/fontawesome-webfont.1dc35d25.ttf"
+  },
+  {
+    "revision": "25a32416abee198dd821b0b17a198a8f",
+    "url": "/retro-board/static/media/fontawesome-webfont.25a32416.eot"
+  },
+  {
+    "revision": "c8ddf1e5e5bf3682bc7bebf30f394148",
+    "url": "/retro-board/static/media/fontawesome-webfont.c8ddf1e5.woff"
+  },
+  {
+    "revision": "d7c639084f684d66a1bc66855d193ed8",
+    "url": "/retro-board/static/media/fontawesome-webfont.d7c63908.svg"
+  },
+  {
+    "revision": "e6cf7c6ec7c2d6f670ae9d762604cb0b",
+    "url": "/retro-board/static/media/fontawesome-webfont.e6cf7c6e.woff2"
+  }
+]);
