347d1d5c6bfb4aa826707a97204f3de328948321
Varun Shrivastava <varunshrivastava007@gmail.com>
2020-09-19 16:24:13 +0530

    dynamic protocol

--- src/env-config.ts
+++ src/env-config.ts
@@ -4,7 +4,7 @@ import User from "./models/User";
 const API_HOST_LOCAL = 'localhost:8082';
 const API_INET_LOCAL = '192.168.1.6:8082';
 const API_HOST_TEST = '139.59.25.205:8082';
-const API_HOST_PROD = '139.59.25.205:8082';
+const API_HOST_PROD = 'retro-service.bemyaficionado.com';
 
 const UI_HOST_LOCAL = 'localhost:3000';
 const UI_INET_LOCAL = '192.168.1.6:3000';
@@ -12,7 +12,7 @@ const UI_HOST_TEST = 'localhost:3001';
 const UI_HOST_PROD = 'retro.bemyaficionado.com';
 const UI_HOST_GITHUB = 'vslala.github.io';
 
-const PROTOCOL = "http://";
+const PROTOCOL = window.location.protocol + "//";
 const HOST_NAMES:Map<string,string> = new Map<string,string>();
 HOST_NAMES.set(UI_HOST_LOCAL, API_HOST_LOCAL);
 HOST_NAMES.set(UI_INET_LOCAL, API_INET_LOCAL);
